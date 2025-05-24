a5c1240a64611d71c51f849a0b6e0870b1344357
JMurphy1 <murphy.johne@gmail.com>
2025-05-23 22:06:43 -0400

    updated script.js

--- backend/public/script.js
+++ backend/public/script.js
@@ -3,7 +3,7 @@ document.getElementById("taskForm").addEventListener("submit", async (e) => {
   const title = document.getElementById("title").value;
   const description = document.getElementById("description").value;
 
-  await fetch("/api/task", {
+  await fetch("/api/tasks", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ title, description }),
