72b013f1129e58e1133529f9d5308d64662961b4
JMurphy1 <murphy.johne@gmail.com>
2025-05-23 21:47:41 -0400

    refactored application

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const tasksRouter = require('./routes/tasks');
+const express = require("express");
+const cors = require("cors");
+const tasksRouter = require("./routes/tasks");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api/tasks', tasksRouter);
+app.use("/api/tasks", tasksRouter);
+const path = require("path");
+
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 module.exports = app;
-
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
